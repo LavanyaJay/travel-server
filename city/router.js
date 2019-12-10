9d4f8df76e690d7a176524154cce9aab68b67225
LavanyaJay <lavanyajay75@gmail.com>
2019-12-10 13:17:12 +0100

    Added city router

--- city/router.js
+++ city/router.js
@@ -1,5 +1,8 @@
 const { Router } = require("express");
 const router = new Router();
 const City = require("./model");
-
+router.get("/city", async (req, res, next) => {
+  const cities = await City.findAll();
+  return res.json(cities);
+});
 module.exports = router;
